d4acd8d49cd7d5ba7d63da0275fc5737d758fc26
Satya <satran004@gmail.com>
2023-09-20 21:25:53 +0800

    chore: Bump yaci store version

--- src/test/java/com/bloxbean/cardano/metdataindexer/MetdataIndexerApplicationTests.java
+++ src/test/java/com/bloxbean/cardano/metdataindexer/MetdataIndexerApplicationTests.java
@@ -3,7 +3,6 @@ package com.bloxbean.cardano.metdataindexer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MetdataIndexerApplicationTests {
 
     @Test
